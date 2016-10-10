12b7ca0fab058fd6adc03ba1942073699aaecd56
UshioShugo <ushio.s@gmail.com>
2016-10-10 11:36:50 +0900

    Finished 1.5

--- 1-5_NumPy/np.py
+++ 1-5_NumPy/np.py
@@ -51,3 +51,39 @@ print(A * 10)
 [[10 20]
  [30 40]]
 '''
+
+
+print('1.5.5　ブロードキャスト')
+A = np.array([[1, 2], [3, 4]])
+B = np.array([10, 20])
+print(A * B)
+'''
+[[10 40]
+ [30 80]]
+'''
+
+print('1.5.6　要素へのアクセス')
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print(X)
+'''
+[[51 55]
+ [14 19]
+ [ 0  4]]
+'''
+print(X[0])    # [51 55]
+print(X[0][1]) # 55
+
+for row in X:
+	print(row)
+'''
+[51 55]
+[14 19]
+[0 4]
+'''
+
+X = X.flatten()
+print(X)                      # [51 55 14 19  0  4]
+print(X[np.array([0, 2, 4])]) # [51 14  0]
+
+print(X > 15)    # [ True  True False  True False False]
+print(X[X > 15]) # [51 55 19]
